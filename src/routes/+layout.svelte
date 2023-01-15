18443a5e97fa86dbeef9d32d666b14d125bb8f15
firebat75 <jacklowry1337@gmail.com>
2023-01-15 14:10:59 -0500

    added header and footer in layout, basic calculator skeleton in main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script></script>
+
+<header>
+    <div>
+        <span class="circle" />
+    </div>
+    <div>
+        <h1>Polytopia Battle Calculator</h1>
+    </div>
+</header>
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <hr />
+    <p>Jack Lowry 2022</p>
+</footer>
+
+<style>
+    main {
+        border: solid rosybrown;
+    }
+    header div {
+        display: inline-block;
+    }
+
+    .circle {
+        height: 25px;
+        width: 25px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+    }
+
+    footer p {
+        text-align: center;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Battle Calculator</title>
+</svelte:head>
+
+<h3>Welcome to SvelteKit</h3>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+frwefesfsdfsfsdfesfsdfsdfdsfsdfwefwgwesfgsdfds
+
+<div id="calculator">
+    <div id="units">
+        <div class="unit">
+            <h2>attacker</h2>
+            <div>
+                <div>
+                    <h3>unit select</h3>
+                </div>
+                <div>
+                    <span class="circle1" />
+                </div>
+            </div>
+        </div>
+        <div class="unit">
+            <h2>defender</h2>
+            <div>
+                <h3>unit select</h3>
+            </div>
+            <div>
+                <span class="circle2" />
+            </div>
+        </div>
+    </div>
+    <button>calculate</button>
+</div>
+
+<style>
+    #calculator {
+        margin: 5px;
+        border: solid red;
+    }
+
+    #units {
+        margin: 5px;
+        border: solid orange;
+    }
+
+    .unit {
+        border: solid lightgray;
+        margin: 5px;
+        display: inline-block;
+    }
+
+    .circle1 {
+        height: 25px;
+        width: 25px;
+        background-color: blue;
+        border-radius: 50%;
+        display: inline-block;
+    }
+
+    .circle2 {
+        height: 25px;
+        width: 25px;
+        background-color: red;
+        border-radius: 50%;
+        display: inline-block;
+    }
+</style>
