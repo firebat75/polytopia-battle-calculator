eaa7905f119d51f3fffa50ec8eedc82a6da0ef24
firebat75 <jacklowry1337@gmail.com>
2023-06-27 16:54:29 -0400

    added skeleton code for a Modal to display combat output

--- src/components/Results.svelte
+++ src/components/Results.svelte
@@ -1,5 +1,7 @@
 <script>
     import { aStats, dStats, dFields, aFields } from "../lib/stores";
+    import Modal from "./Modal.svelte";
+    let showModal = false;
 
     function calcAttackForce() {
         return $aStats.attack * ($aStats.health / $aStats.maxHealth);
@@ -45,7 +47,34 @@
     <p>{JSON.stringify($dFields)}</p>
     <h1>{td}</h1>
     <button on:click={calculate}>calculate</button>
+    <button on:click={() => (showModal = true)}> show modal </button>
 </div>
+
+<Modal bind:showModal>
+    <h2 slot="header">
+        modal
+        <small><em>adjective</em> mod·al \ˈmō-dəl\</small>
+    </h2>
+
+    <ol class="definition-list">
+        <li>of or relating to modality in logic</li>
+        <li>
+            containing provisions as to the mode of procedure or the manner of
+            taking effect —used of a contract or legacy
+        </li>
+        <li>of or relating to a musical mode</li>
+        <li>of or relating to structure as opposed to substance</li>
+        <li>
+            of, relating to, or constituting a grammatical form or category
+            characteristically indicating predication
+        </li>
+        <li>of or relating to a statistical mode</li>
+    </ol>
+
+    <a href="https://www.merriam-webster.com/dictionary/modal"
+        >merriam-webster.com</a
+    >
+</Modal>
 
 <style>
     .main {
